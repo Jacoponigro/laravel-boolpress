f724be268581ab36f60a4d818be6b8a03a72e029
Jacopo <jakonigro@gmail.com>
2020-11-09 17:17:02 +0100

    create colonne tabella post e popolate con faker

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Post;
+use App\User;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +13,16 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $user = User::inRandomOrder()->first();
+            $newPost = new Post;
+            $newPost->user_id = $user->id;
+            $newPost->title = $faker->sentence(6, true);
+            $newPost->content = $faker->paragraph(6, true);
+            $newPost->slug = Str::of($newPost->title)->slug("-");
+            $newPost->save();
+        }
     }
 }
